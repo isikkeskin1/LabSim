246280a837d60cc1f290d3667f0650b33643ef7c
olvz <43779905+isikkeskin1@users.noreply.github.com>
2026-09-12 03:39:43 +0200

    Expose experiment analysis toolkit

--- labsim/__init__.py
+++ labsim/__init__.py
@@ -3,14 +3,15 @@
 from .analysis import max_state_error
 from .config_io import load_config, save_config
 from .convergence import ConvergencePoint, ConvergenceStudy, run_convergence_study
+from .events import first_crossing, first_event
 from .experiments import ExperimentConfig, run_experiment
 from .io import write_csv
-from .metrics import final_state_error, root_mean_square
-from .models import DampedOscillator, HarmonicOscillator, LogisticGrowth, LotkaVolterra, ODEModel
+from .metrics import final_state_error, root_mean_square, state_range, time_average
+from .models import DampedOscillator, HarmonicOscillator, LogisticGrowth, LotkaVolterra, ODEModel, SIRModel, SimplePendulum
 from .physics import harmonic_energy, relative_drift
 from .sensitivity import central_difference
 from .solvers import ODESolution, integrate_ode
-from .sweeps import SweepResult, parameter_sweep
+from .sweeps import SweepResult, SweepSummary, parameter_sweep, summarize_sweep
 
 __all__ = [
     "ConvergencePoint",
@@ -22,9 +23,14 @@ __all__ = [
     "LotkaVolterra",
     "ODEModel",
     "ODESolution",
+    "SIRModel",
+    "SimplePendulum",
     "SweepResult",
+    "SweepSummary",
     "central_difference",
     "final_state_error",
+    "first_crossing",
+    "first_event",
     "harmonic_energy",
     "integrate_ode",
     "load_config",
@@ -35,5 +41,8 @@ __all__ = [
     "run_convergence_study",
     "run_experiment",
     "save_config",
+    "state_range",
+    "summarize_sweep",
+    "time_average",
     "write_csv",
 ]
