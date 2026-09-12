588b613d934b0036046ade84b904c8619618e7a1
olvz <43779905+isikkeskin1@users.noreply.github.com>
2026-09-12 03:33:20 +0200

    Expose physical model utilities

--- labsim/__init__.py
+++ labsim/__init__.py
@@ -2,16 +2,20 @@
 
 from .analysis import max_state_error
 from .convergence import ConvergencePoint, ConvergenceStudy, run_convergence_study
-from .models import HarmonicOscillator, ODEModel
+from .models import DampedOscillator, HarmonicOscillator, ODEModel
+from .physics import harmonic_energy, relative_drift
 from .solvers import ODESolution, integrate_ode
 
 __all__ = [
     "ConvergencePoint",
     "ConvergenceStudy",
+    "DampedOscillator",
     "HarmonicOscillator",
     "ODEModel",
     "ODESolution",
+    "harmonic_energy",
     "integrate_ode",
     "max_state_error",
+    "relative_drift",
     "run_convergence_study",
 ]
