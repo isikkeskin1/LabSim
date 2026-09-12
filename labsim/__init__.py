b1cdc4bc8832f1e2ef37a958b181fdeff51dfa9e
olvz <43779905+isikkeskin1@users.noreply.github.com>
2026-09-12 03:31:55 +0200

    Expose numerical error analysis

--- labsim/__init__.py
+++ labsim/__init__.py
@@ -1,6 +1,13 @@
 """Core numerical tools for LabSim."""
 
+from .analysis import max_state_error
 from .models import HarmonicOscillator, ODEModel
 from .solvers import ODESolution, integrate_ode
 
-__all__ = ["HarmonicOscillator", "ODEModel", "ODESolution", "integrate_ode"]
+__all__ = [
+    "HarmonicOscillator",
+    "ODEModel",
+    "ODESolution",
+    "integrate_ode",
+    "max_state_error",
+]
