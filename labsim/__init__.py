6c28b5e2ed3044d44f0fe0da941149cb9c7565cb
olvz <43779905+isikkeskin1@users.noreply.github.com>
2026-09-12 03:33:34 +0200

    Expose trajectory export API

--- labsim/__init__.py
+++ labsim/__init__.py
@@ -2,6 +2,7 @@
 
 from .analysis import max_state_error
 from .convergence import ConvergencePoint, ConvergenceStudy, run_convergence_study
+from .io import write_csv
 from .models import DampedOscillator, HarmonicOscillator, ODEModel
 from .physics import harmonic_energy, relative_drift
 from .solvers import ODESolution, integrate_ode
@@ -18,4 +19,5 @@ __all__ = [
     "max_state_error",
     "relative_drift",
     "run_convergence_study",
+    "write_csv",
 ]
