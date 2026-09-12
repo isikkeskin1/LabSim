354e205f971be497c9fe78091bba0bd824cb5fb8
olvz <43779905+isikkeskin1@users.noreply.github.com>
2026-09-12 03:32:42 +0200

    Expose convergence analysis API

--- labsim/__init__.py
+++ labsim/__init__.py
@@ -1,13 +1,17 @@
 """Core numerical tools for LabSim."""
 
 from .analysis import max_state_error
+from .convergence import ConvergencePoint, ConvergenceStudy, run_convergence_study
 from .models import HarmonicOscillator, ODEModel
 from .solvers import ODESolution, integrate_ode
 
 __all__ = [
+    "ConvergencePoint",
+    "ConvergenceStudy",
     "HarmonicOscillator",
     "ODEModel",
     "ODESolution",
     "integrate_ode",
     "max_state_error",
+    "run_convergence_study",
 ]
