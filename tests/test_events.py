337af3c884c1a26e51c63f10591102a13d754be6
olvz <43779905+isikkeskin1@users.noreply.github.com>
2026-09-13 03:38:39 +0200

    Test localized event detection

--- tests/test_events.py
+++ tests/test_events.py
@@ -1,5 +1,5 @@
 from labsim import integrate_ode
-from labsim.events import first_crossing, first_event
+from labsim.events import first_crossing, first_crossing_linear, first_event
 
 
 def make_ramp():
@@ -27,3 +27,18 @@ def test_first_crossing_detects_downward_crossing():
 def test_first_crossing_returns_none_when_no_crossing_exists():
     solution = integrate_ode(lambda _time, _state: (1.0,), (1.0,), dt=0.5, steps=2)
     assert first_crossing(solution, 0, 0.0) is None
+
+
+def test_linear_crossing_localizes_between_samples():
+    solution = integrate_ode(lambda _time, _state: (1.0,), (-0.3,), dt=0.2, steps=4)
+    event = first_crossing_linear(solution, 0, 0.0)
+    assert event is not None
+    assert event[0] == 0.3
+    assert event[1][0] == 0.0
+
+
+def test_linear_crossing_can_preserve_sampled_behavior():
+    solution = integrate_ode(lambda _time, _state: (1.0,), (-0.3,), dt=0.2, steps=4)
+    event = first_crossing_linear(solution, 0, 0.0, interpolate=False)
+    assert event is not None
+    assert event[0] == 0.4
