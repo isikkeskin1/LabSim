0895d94ae995affbc971e18e606fdcb1753f4dc4
olvz <43779905+isikkeskin1@users.noreply.github.com>
2026-09-13 03:38:43 +0200

    Expose localized event API

--- labsim/__init__.py
+++ labsim/__init__.py
@@ -3,47 +3,22 @@
 from .analysis import max_state_error
 from .config_io import load_config, save_config
 from .convergence import ConvergencePoint, ConvergenceStudy, run_convergence_study
-from .events import first_crossing, first_event
+from .events import first_crossing, first_crossing_linear, first_event
 from .experiments import ExperimentConfig, run_experiment
 from .io import write_csv
 from .metrics import final_state_error, root_mean_square, state_range, time_average
 from .models import DampedOscillator, HarmonicOscillator, LogisticGrowth, LotkaVolterra, ODEModel, SIRModel, SimplePendulum
 from .physics import harmonic_energy, relative_drift
 from .sensitivity import central_difference
-from .solvers import ODESolution, integrate_adaptive, integrate_ode
+from .solvers import ODESolution, integrate_ode
 from .sweeps import SweepResult, SweepSummary, parameter_sweep, summarize_sweep
 
 __all__ = [
-    "ConvergencePoint",
-    "ConvergenceStudy",
-    "DampedOscillator",
-    "ExperimentConfig",
-    "HarmonicOscillator",
-    "LogisticGrowth",
-    "LotkaVolterra",
-    "ODEModel",
-    "ODESolution",
-    "SIRModel",
-    "SimplePendulum",
-    "SweepResult",
-    "SweepSummary",
-    "central_difference",
-    "final_state_error",
-    "first_crossing",
-    "first_event",
-    "harmonic_energy",
-    "integrate_adaptive",
-    "integrate_ode",
-    "load_config",
-    "max_state_error",
-    "parameter_sweep",
-    "relative_drift",
-    "root_mean_square",
-    "run_convergence_study",
-    "run_experiment",
-    "save_config",
-    "state_range",
-    "summarize_sweep",
-    "time_average",
-    "write_csv",
+    "ConvergencePoint", "ConvergenceStudy", "DampedOscillator", "ExperimentConfig",
+    "HarmonicOscillator", "LogisticGrowth", "LotkaVolterra", "ODEModel", "ODESolution",
+    "SIRModel", "SimplePendulum", "SweepResult", "SweepSummary", "central_difference",
+    "final_state_error", "first_crossing", "first_crossing_linear", "first_event",
+    "harmonic_energy", "integrate_ode", "load_config", "max_state_error", "parameter_sweep",
+    "relative_drift", "root_mean_square", "run_convergence_study", "run_experiment",
+    "save_config", "state_range", "summarize_sweep", "time_average", "write_csv",
 ]
