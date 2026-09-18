5caf17a43233129d8e4640ef0d676ae7342f6e42
olvz <43779905+isikkeskin1@users.noreply.github.com>
2026-09-18 03:11:00 +0200

    Expose uncertainty sampling API

--- labsim/__init__.py
+++ labsim/__init__.py
@@ -14,14 +14,24 @@ from .physics import harmonic_energy, relative_drift
 from .sensitivity import central_difference
 from .solvers import ODESolution, integrate_adaptive, integrate_ode
 from .sweeps import SweepResult, SweepSummary, parameter_sweep, summarize_sweep
-from .uncertainty import EnsembleMember, EnsembleStatistics, ensemble_statistics, run_ensemble
+from .uncertainty import (
+    EnsembleMember,
+    EnsembleQuantiles,
+    EnsembleStatistics,
+    NormalDistribution,
+    UniformDistribution,
+    ensemble_quantiles,
+    ensemble_statistics,
+    run_ensemble,
+    sample_parameters,
+)
 
 __all__ = [
-    "ConvergencePoint", "ConvergenceStudy", "DampedOscillator", "EnsembleMember", "EnsembleStatistics", "ExperimentConfig",
-    "HarmonicOscillator", "LogisticGrowth", "LotkaVolterra", "ODEModel", "ODESolution",
-    "SIRModel", "SimplePendulum", "SweepResult", "SweepSummary", "central_difference", "ensemble_statistics",
+    "ConvergencePoint", "ConvergenceStudy", "DampedOscillator", "EnsembleMember", "EnsembleQuantiles", "EnsembleStatistics", "ExperimentConfig",
+    "HarmonicOscillator", "LogisticGrowth", "LotkaVolterra", "NormalDistribution", "ODEModel", "ODESolution",
+    "SIRModel", "SimplePendulum", "SweepResult", "SweepSummary", "UniformDistribution", "central_difference", "ensemble_quantiles", "ensemble_statistics",
     "final_state_error", "first_crossing", "first_crossing_hermite", "first_crossing_linear", "first_event",
     "harmonic_energy", "integrate_adaptive", "integrate_ode", "integrate_rk23", "load_config", "max_state_error",
     "parameter_sweep", "relative_drift", "resample_uniform", "root_mean_square", "run_convergence_study",
-    "run_ensemble", "run_experiment", "sample_hermite", "save_config", "state_range", "summarize_sweep", "time_average", "write_csv",
+    "run_ensemble", "run_experiment", "sample_hermite", "sample_parameters", "save_config", "state_range", "summarize_sweep", "time_average", "write_csv",
 ]
